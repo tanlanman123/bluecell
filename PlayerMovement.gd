4746c063a1f649fcd8893589cb882f292bc1e45f
tanlanman123 <tannerhawk2012@gmail.com>
2026-06-19 14:34:49 -0400

    player can move
    added script and the player can move with arrow keys and jump with up arrow key :)

---
+++ PlayerMovement.gd
@@ -0,0 +1,20 @@
+extends CharacterBody2D
+
+@export var speed: float = 250.0
+@export var jump_velocity: float = -400.0
+@export var gravity: float = 900.0
+
+func _physics_process(delta):
+	# Add gravity when the player is in the air.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	# Move left and right with arrow keys.
+	var direction = Input.get_axis("ui_left", "ui_right")
+	velocity.x = direction * speed
+
+	# Jump when the player presses the up arrow and is on the floor.
+	if Input.is_action_just_pressed("ui_up") and is_on_floor():
+		velocity.y = jump_velocity
+
+	move_and_slide()
