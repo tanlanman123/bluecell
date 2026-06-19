9c3635e8c03e3bca31cdc497dba87b914ad501a8
tanlanman123 <tannerhawk2012@gmail.com>
2026-06-19 16:20:12 -0400

    made controller work
    the controller works now and the jump button is the bottom action and on keyboard it is z nothing about the movement has changed

--- PlayerMovement.gd
+++ PlayerMovement.gd
@@ -1,11 +1,11 @@
 extends CharacterBody2D
 
 @export var speed: float = 250.0
-@export var jump_velocity: float = -400.0
+@export var jump_velocity: float = -400.0 #up is negative for some reason
 @export var gravity: float = 900.0
 
 func _physics_process(delta):
-	# Add gravity when the player is in the air.
+	# Add gravity when the player is in the air
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
@@ -13,8 +13,8 @@ func _physics_process(delta):
 	var direction = Input.get_axis("ui_left", "ui_right")
 	velocity.x = direction * speed
 
-	# Jump when the player presses the up arrow and is on the floor.
-	if Input.is_action_just_pressed("ui_up") and is_on_floor():
+	# Jump when the player presses the "z" button and is on the floor
+	if Input.is_action_just_pressed("press_z") and is_on_floor():
 		velocity.y = jump_velocity
 
 	move_and_slide()
