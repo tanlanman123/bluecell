2b2d267d10c9203a338ae09ee6973780200e0e08
tanlanman123 <tannerhawk2012@gmail.com>
2026-06-21 06:49:19 -0400

    you can now cut jump

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,9 +1,8 @@
 extends CharacterBody2D
 
 
-const SPEED = 250.0
-const JUMP_VELOCITY = -300.0
-
+var SPEED = 250.0
+var JUMP_VELOCITY = -500.0
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -11,11 +10,12 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("press_z") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+	if Input.is_action_just_released("press_z") and velocity.y < 0.0:
+		velocity.y = 0
 
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
